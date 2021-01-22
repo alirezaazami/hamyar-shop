09bdd459219a6ada5e8b575b07e94b0260067aca
alireza azami <alireza.azami@gmail.com>
2021-01-22 19:24:06 +0330

    section 2 (start theme)

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alireza azami (hamyar.co)
+ * Date: ۲۲/۰۱/۲۰۲۱
+ * Time: ۰۵:۰۵ بعدازظهر
+ */
+
+echo 'home.php';
---
+++ search.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alireza azami (hamyar.co)
+ * Date: ۲۲/۰۱/۲۰۲۱
+ * Time: ۰۵:۰۷ بعدازظهر
+ */
+
