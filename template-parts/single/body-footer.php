7546e1a69d87058ac52d90d5ceb4ec65ecdc2280
alireza azami <alireza.azami@gmail.com>
2021-03-03 07:51:33 +0330

    add loop to single

--- template-parts/single/body-footer.php
+++ template-parts/single/body-footer.php
@@ -1,11 +1,22 @@
 <div class="d-flex justify-content-between align-items-center my-5 arrows">
-    <a href="#"> <i class="fas fa-chevron-right align-middle ml-1"></i>بعدی </a>
-    <a href="#"> قبلی<i class="fas fa-chevron-left align-middle mr-1"></i> </a>
+    <?php if (get_next_post_link()): ?>
+    <div class="next-post-link">
+        <i class="fas fa-chevron-right align-middle ml-1"></i>
+        <?php next_post_link('%link','next post'); ?>
+    </div>
+    <?php endif; ?>
+    <?php if (get_previous_post_link()): ?>
+    <div class="previous-post-link">
+        <?php previous_post_link('%link','previous post'); ?>
+        <i class="fas fa-chevron-left align-middle mr-1"></i>
+    </div>
+    <?php endif; ?>
 </div>
 <div class="d-felx justify-content-between align-items-center mb-4">
     <strong class="d-block"> برچسب ها :
-        <a href="#" class="px-3 py-1 rounded-pill bg-light"> لوازم </a>
-        <a href="#" class="px-3 py-1 rounded-pill bg-light"> طراحی </a>
+        <?php the_tags(); ?>
+<!--        <a href="#" class="px-3 py-1 rounded-pill bg-light"> لوازم </a>-->
+<!--        <a href="#" class="px-3 py-1 rounded-pill bg-light"> طراحی </a>-->
     </strong>
 </div>
 <?php   comments_template();
