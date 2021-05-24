c79daeb19857bb6c00a692e4d25dcec9f2959039
alireza <alireza.azami@gmail.com>
2021-05-24 14:43:49 +0430

    start home page

--- home.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: alireza azami (hamyar.co)
- * Date: ۲۲/۰۱/۲۰۲۱
- * Time: ۰۵:۰۵ بعدازظهر
- */
-
-echo 'home.php';
--- templates/front-1.php
+++ templates/front-1.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * template name: font page 1
+ */
+
+?>
 <?php   get_header(); ?>
 
     <!-- Start Main -->
